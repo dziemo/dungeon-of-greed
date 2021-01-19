449f1cefec09656607a542cf8ec3742ef7331a53
Dziemo <michalek210@gmail.com>
2021-01-19 15:34:25 +0100

    Character move, look and basic interact

--- Assets/Scripts/Gameplay/GameplayCharacterController.cs
+++ Assets/Scripts/Gameplay/GameplayCharacterController.cs
@@ -10,9 +10,13 @@ public class GameplayCharacterController : MonoBehaviour
     public Transform armRightPos;
 
     public float characterSpeed = 5f;
+    public float itemCheckRadius = 2f;
+    public LayerMask interactablesLayer;
 
     Rigidbody rb;
     Vector3 moveDir;
+    Vector3 mousePoint;
+    Interactable interactableItem;
 
     private void Start()
     {
@@ -27,13 +31,51 @@ public class GameplayCharacterController : MonoBehaviour
         Instantiate(player.characterBody.armRightPrefab, armRightPos);
     }
 
+    public void LookGamepad (Vector3 dir)
+    {
+        transform.forward = new Vector3(dir.x, 0, dir.y);
+    }
+
+    public void LookMouse(Vector3 point)
+    {
+        mousePoint = point;
+    }
+
     public void Move (Vector3 dir)
     {
         moveDir = new Vector3(dir.x, 0, dir.y);
     }
 
+    public void Interact ()
+    {
+        if (interactableItem)
+        {
+            interactableItem.Interact();
+        }
+    }
+
     private void Update()
     {
+        rb.MoveRotation(Quaternion.LookRotation((mousePoint - transform.position).normalized, Vector3.up));
         rb.MovePosition(rb.position + (moveDir * characterSpeed * Time.deltaTime));
+        InteractableCheck();
+    }
+
+    private void InteractableCheck()
+    {
+        var interactables = Physics.OverlapSphere(transform.position, itemCheckRadius, interactablesLayer);
+        if (interactables.Length > 0)
+        {
+            interactableItem = interactables[0].GetComponent<Interactable>();
+        }
+        else
+        {
+            interactableItem = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, itemCheckRadius);
     }
 }
--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -6,11 +6,62 @@ using UnityEngine.InputSystem;
 public class PlayerInputController : MonoBehaviour
 {
     public GameplayCharacterController characterController;
-    Vector2 moveDir;
+    public LayerMask groundMask;
+
+    bool mouseController;
+    Camera cam;
+    Vector3 mousePos;
+
+    PlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        
+        if (playerInput.devices[0].device.displayName == "Mouse")
+        {
+            mouseController = true;
+            cam = Camera.main;
+        }
+    }
 
     private void OnMove(InputValue value)
     {
-        moveDir = value.Get<Vector2>();
+        var moveDir = value.Get<Vector2>();
         characterController.Move(moveDir);
+    }
+
+    private void OnLook(InputValue value)
+    {
+        if (mouseController)
+        {
+            mousePos = value.Get<Vector2>();
+        }
+        else
+        {
+            var lookDir = value.Get<Vector2>();
+            characterController.LookGamepad(lookDir);
+        }
+    }
+
+    private void OnInteract ()
+    {
+        characterController.Interact();
+    }
+
+    private void Update()
+    {
+        if (mouseController)
+        {
+            Vector3 tempMousePos = cam.ScreenToWorldPoint(mousePos - new Vector3(0, 0, cam.transform.position.z));
+            Ray mouseRay = new Ray(cam.transform.position, tempMousePos - cam.transform.position);
+            RaycastHit mouseRayHit;
+            Debug.DrawRay(mouseRay.origin, mouseRay.direction, Color.red, 1f);
+
+            if (Physics.Raycast(mouseRay, out mouseRayHit, 100f, groundMask))
+            {
+                characterController.LookMouse(mouseRayHit.point);
+            }
+        }
     }
 }
